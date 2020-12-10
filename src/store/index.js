613af78445228b0a28a0bfd0c4368c3818710959
agnespravida <agnespravida27@gmail.com>
2020-12-10 20:27:54 +0700

    creating game page

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    answer: null
   },
   mutations: {
+    getAnswer (state, payload) {
+      state.answer = payload
+    }
   },
   actions: {
   },
